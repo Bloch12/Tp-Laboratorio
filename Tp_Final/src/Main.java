5dc5d90d4831b0f917048d66a83e2f4f025528aa
Nahuel Bianca <nahuelbianca90@gmail.com>
2023-05-26 16:48:34 -0300

    inicio de cargar un pokemon

--- Tp_Final/src/Main.java
+++ Tp_Final/src/Main.java
@@ -1,7 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         Pokedex.cargarPokemones();
-        Integer num = 156;
-        System.out.println(Pokedex.buscarPokeom(num.toString()));
+        System.out.println(Pokedex.buscarPokeom("grookey"));
     }
 }
--- Tp_Final/src/PokemonDatos.java
+++ Tp_Final/src/PokemonDatos.java
@@ -1,8 +1,27 @@
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
 public class PokemonDatos extends Pokemon{
     private int numPokedex;
+    private ArrayList<String> habilidades;
+    private String sprite;
+    private ArrayList<String> tipos;
+
     public PokemonDatos(String url, String nombre) {
         super(url, nombre);
         numPokedex = 0;
+    }
+
+    public void Cargar(){
+        JSONObject jsonObject;
+        try {
+            jsonObject = new JSONObject(ConsumeApi.getInfo(getUrl()));
+        }catch(JSONException e){
+            jsonObject = null;
+        }
+
     }
 
 
