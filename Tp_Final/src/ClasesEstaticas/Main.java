b39a2221f6ad2fae0ce8c238b9fedf47ea21d8e8
TomasCorso <tomicorso04@gmail.com>
2023-06-02 17:54:36 -0300

    agregue un sout
    hola

--- Tp_Final/src/ClasesEstaticas/Main.java
+++ Tp_Final/src/ClasesEstaticas/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         Pokedex.cargarPokemones();
         System.out.println(Pokedex.buscarPokemon("pikachu"));
+        System.out.println("Probar algo");
     }
 }
