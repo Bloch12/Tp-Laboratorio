84f8327f5c46c6258dc77c1825d0a155082b7306
Nahuel Bianca <nahuelbianca90@gmail.com>
2023-06-02 17:55:53 -0300

    remover el sout

--- Tp_Final/src/ClasesEstaticas/Main.java
+++ Tp_Final/src/ClasesEstaticas/Main.java
@@ -4,6 +4,5 @@ public class Main {
     public static void main(String[] args) {
         Pokedex.cargarPokemones();
         System.out.println(Pokedex.buscarPokemon("pikachu"));
-        System.out.println("Probar algo");
     }
 }
